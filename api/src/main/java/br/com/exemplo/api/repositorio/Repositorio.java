4e4a626d5c34b71f82437695569651d609fe1935
Tijolinho529 <161033643+Tijolinho529@users.noreply.github.com>
2025-05-29 10:59:56 -0300

    Add files via upload

---
+++ api/src/main/java/br/com/exemplo/api/repositorio/Repositorio.java
@@ -0,0 +1,16 @@
+package br.com.exemplo.api.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.exemplo.api.modelo.Pessoa;
+
+@Repository
+public interface Repositorio extends CrudRepository<Pessoa, Integer> {
+    List<Pessoa> findAll();
+
+    Pessoa findByCodigo(int codigo);
+   
+}
